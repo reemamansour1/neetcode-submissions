c77582aaf8d2ae43c6bbfb1a81128575d7d61346
Reema Mansour <66185445+reemamansour1@users.noreply.github.com>
2026-09-23 09:45:15 -0400

    Add: level-order-traversal-of-binary-tree - submission-2

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<TreeNode> q = new ArrayDeque<>();
+        if(root != null) q.offer(root);
+        while(!q.isEmpty()){
+            List<Integer> level = new ArrayList<>();
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode n = q.poll();
+                level.add(n.val);
+                if(n.left != null){
+                    q.offer(n.left);
+                }
+                if(n.right != null){
+                    q.offer(n.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+        
+    }
+
+}
